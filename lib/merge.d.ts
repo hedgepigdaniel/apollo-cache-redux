7d93699467448598eb4e281728f6d12a779f8fe5
Daniel Playfair Cal <daniel.playfair.cal@gmail.com>
2018-05-05 22:30:34 +1000

    Build

---
+++ lib/merge.d.ts
@@ -0,0 +1,3 @@
+export declare function weCareAbout(val: any): boolean;
+export declare function assoc<T extends object | [any], V>(coll: T, key: string | number, value: V): T;
+export declare function merge<T, S1>(target: T, source: S1): (T & S1);
